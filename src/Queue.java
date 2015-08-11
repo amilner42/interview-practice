4893f88ce5df907b3389ff7273e4e1215c6db91d
Arie Milner <amilner42@gmail.com>
2015-08-11 09:11:03 -0700

    Basic Queue class implemented with circ array

---
+++ src/Queue.java
@@ -0,0 +1,70 @@
+import java.util.EmptyStackException;
+
+// An implementation of a Queue using a circular array
+public class Queue {
+	
+	private int[] array;
+	private int head , tail, currentSize; 
+	
+	public Queue(int maxSize) {
+		array = new int[maxSize];
+		head = -1;
+		tail = -1; 
+	}
+	
+	// O(1)
+	public void enQueue(int data) {
+		if(this.isFull()) {
+			throw new StackOverflowError();
+		} else {
+			tail = increment(tail);
+			array[tail] = data; 
+			currentSize++;			
+		}
+	}
+	
+	// O(1)
+	public int deQueue() {
+		if(this.isEmpty()) {
+			throw new EmptyStackException(); 
+		} else {
+			head = increment(head);
+			int item = array[head];
+			currentSize--;
+			return item;
+		}
+	}
+	
+	// O(1)
+	public boolean isEmpty() {
+		return currentSize == 0;
+	}
+	
+	// O(1)
+	public boolean isFull() {
+		return currentSize == array.length;
+	}
+	
+	public String toString() {
+		String result = "{ ";
+		int copySize = currentSize; 
+		int headCopy = head; 
+		
+		while(copySize-- > 0) {
+			headCopy = increment(headCopy);
+			result += array[headCopy] + " ";
+		}
+		
+		result += "}";
+		return result; 
+	}
+	
+	private int increment(int index) {
+		if(++index == array.length) {
+			index = 0; 
+		}
+		return index; 
+	}
+	
+	
+}
---
+++ src/QueueDemo.java
@@ -0,0 +1,44 @@
+
+public class QueueDemo {
+	
+	public static void main(String[] args) {
+		Queue queue = new Queue(4);
+		System.out.println(queue.isEmpty());
+		System.out.println(queue.toString());
+		
+		queue.enQueue(5);
+		queue.enQueue(7);
+		queue.enQueue(10);
+		
+		System.out.println(queue.toString());
+		System.out.println(queue.toString());
+		
+		queue.enQueue(12);
+		
+		System.out.println(queue.toString());
+		System.out.println(queue.isFull());
+		
+		try {
+			queue.enQueue(15);
+		} catch (Error e) {
+			System.out.println(e);
+		}
+		
+		queue.deQueue();
+		queue.deQueue();
+		
+		System.out.println(queue.toString());
+		
+		queue.deQueue();
+		queue.deQueue();
+		
+		try {
+			queue.deQueue();	
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		System.out.println(queue.toString());
+	}
+
+}
