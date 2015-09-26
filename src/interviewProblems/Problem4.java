622700e6090bc698767d6cd602d8f4c2cfc0a6c7
Arie Milner <amilner42@gmail.com>
2015-09-26 16:41:18 -0700

    Implemented Problem 4

---
+++ src/interviewProblems/Problem4.java
@@ -0,0 +1,58 @@
+package interviewProblems;
+
+import java.util.Arrays;
+
+public class Problem4 {
+
+    public static void main(String[] args) {
+
+        int[] data1 = new int[]{};   				// --> exception
+        int[] data2 = new int[]{2};					// --> exception
+        int[] data3 = new int[]{1 , 5};				// --> {5 , 1}
+        int[] data4 = new int[]{1 , 2 , 3 , 4};		// --> {2 * 3 * 4 , 1 * 3 * 4 ,  1 * 2 * 4, 1 * 2 * 3}
+
+        try {
+//			System.out.println(Arrays.toString(productAllButMe(data1)));
+            System.out.println(Arrays.toString(productAllButMe(data2)));
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Arrays.toString(productAllButMe(data3)));
+            System.out.println(Arrays.toString(productAllButMe(data4)));
+        } catch(Exception e) {
+            System.out.println("You should not be seeing this text!");
+        }
+    }
+
+    private static int[] productAllButMe(int[] data) throws ArrayTooSmallException {
+
+        int[] result = new int[data.length];
+
+        if(data.length < 2) {
+            throw new ArrayTooSmallException("Array must at minimum have two elements to preform productAllButMe(int[] data)");
+        }
+
+        int recordMultiplication = 1;
+        for(int i = 0; i < data.length; i++) {
+            result[i] = recordMultiplication;
+            recordMultiplication *= data[i];
+        }
+
+        recordMultiplication = 1;
+        for(int i = data.length - 1; i >= 0; i--) {
+            result[i] *= recordMultiplication;
+            recordMultiplication *= data[i];
+        }
+
+        return result;
+    }
+}
+
+class ArrayTooSmallException extends Exception {
+
+    public ArrayTooSmallException(String info) {
+        super(info);
+    }
+}
